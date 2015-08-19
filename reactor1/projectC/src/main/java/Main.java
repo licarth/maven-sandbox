1de22f4f66ac1f0a24e267a404dcac9a8ff74550
thomas <thomas.carli@goeuro.com>
2015-08-19 11:15:38 +0200

    init

---
+++ reactor1/projectC/src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.google.common.base.Stopwatch;
+
+public class Main {
+    public static void main(String[] args) {
+        new Stopwatch(); //V12.0
+    }
+}
---
+++ reactor2/projectD/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.google.common.base.Stopwatch;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stopwatch.createStarted(); //V18.0
+    }
+}
